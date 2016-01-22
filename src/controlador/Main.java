4fb29b3808c83a30bb2d4c17fdc86e9d0007f39a
Damián Ariel Betancur <da_ariel@hotmail.com>
2016-01-22 19:46:44 -0300

    actualizacion

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -16,8 +16,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //muestraContenido("C:/Users/Ariel/Documents/NetBeansProjects/LectorArchivoTexto/MisOrdenes17102015024452PDAS.TXT");
         //String rutaArchivo = "C:/Users/Ariel/Documents/NetBeansProjects/LectorArchivoTexto/MisOrdenes17102015024452PDAS.TXT";
-        String rutaArchivo ="C:/Users/DamiánAriel/Documents/GitHub/lectorOrdenes/MisOrdenes17102015024452PDAS.TXT";
+        String rutaArchivo ="C:/Users/Damián Ariel Betancu/Documents/GitHub/Nueva Carpeta/lectorOrdenes/MisOrdenes17102015024452PDAS.TXT";
         LectorArchivoTexto l1 = new LectorArchivoTexto(rutaArchivo);
-        l1.validarNumeroDeOrden();
+        
+        
+        System.out.println(l1.getOrdenes().size());
     }
 }
--- src/modelo/LectorArchivoTexto.java
+++ src/modelo/LectorArchivoTexto.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Clase encargada de leer un archivo 
@@ -15,7 +16,7 @@ import java.io.IOException;
  */
 public class LectorArchivoTexto {
     String archivo;
-    private OrdenMBA orden;
+    private ArrayList <OrdenMBA>  ordenes;
     
     /**
      * Instancia un Objeto LectorArchivo 
@@ -25,15 +26,24 @@ public class LectorArchivoTexto {
      */
     public LectorArchivoTexto(String RutaArchivo) throws FileNotFoundException, IOException{
         this.archivo = RutaArchivo;
-       
+        ordenes = new ArrayList();
+        cargarOrdenes();        
     }
-    
+        
+    /**
+     * Muesta la lista de Ordenes
+     * @return ArrayList <OrdenMBA>
+     */
+    public ArrayList <OrdenMBA> getOrdenes() {
+        return ordenes;
+    }
+        
     /**
      * Muesta el contenido de un archivo de texto
      * @throws FileNotFoundException
      * @throws IOException 
      */
-    public void muestraContenido() throws FileNotFoundException, IOException {
+    public void muestrarContenido() throws FileNotFoundException, IOException {
         String cadena;
         FileReader f = new FileReader(this.archivo);
         BufferedReader b = new BufferedReader(f);
@@ -48,7 +58,7 @@ public class LectorArchivoTexto {
      * @throws FileNotFoundException
      * @throws IOException 
      */
-    public void contadorRegistro() throws FileNotFoundException, IOException {
+    public void contarRegistros() throws FileNotFoundException, IOException {
         int contador = 0;
         String cadena;        
         FileReader f = new FileReader(this.archivo);
@@ -65,7 +75,7 @@ public class LectorArchivoTexto {
      * @throws FileNotFoundException
      * @throws IOException 
      */
-    public void contadorLongitudPorRegistro() throws FileNotFoundException, IOException {
+    public void MostrarLongitudRegistros() throws FileNotFoundException, IOException {
         int contador = 0;
         String cadena;        
         FileReader f = new FileReader(this.archivo);
@@ -78,48 +88,63 @@ public class LectorArchivoTexto {
     }    
 
     /**
-     * Valida el numero de Orden del archivo de Exportacion
-     * posicion inicial 1
-     * posicion final 6
+     * Carga cada linea como un Objeto, guardando los mismos en una lista de Objetos del tipo Ordene
      * @throws FileNotFoundException
      * @throws IOException 
      */
-    public void validarNumeroDeVIN() throws FileNotFoundException, IOException {        
-        String cadena;        
-        FileReader f = new FileReader(this.archivo);
-        BufferedReader b = new BufferedReader(f);
-        while((cadena = b.readLine())!=null) {            
-        System.out.println(cadena.substring(0, 6));   
-        }
-        b.close();
-    } 
-        
- 
-    /**
-     * Valida el numero de ORDEN del archivo de Exportacion
-     * posicion inicial 0
-     * posicion final 6
-     * @throws FileNotFoundException
-     * @throws IOException 
-     */
-    public void validarNumeroDeOrden() throws FileNotFoundException, IOException {                
-        String cadena;        
+    private void cargarOrdenes() throws FileNotFoundException, IOException {
+        String cadena;         
         FileReader f = new FileReader(this.archivo);
         BufferedReader b = new BufferedReader(f);
         while((cadena = b.readLine())!=null) {  
-            if (Integer.parseInt(cadena.substring(1,6))>=22000&&Integer.parseInt(cadena.substring(1,6))<=22500) {
-                System.out.println(cadena.substring(0, 6)); 
-            }else{
-                System.out.println(cadena.substring(0, 6)+"  fuera de rango");  
-            }        
+           OrdenMBA OrdenAuxiliar = new OrdenMBA();
+           OrdenAuxiliar.setNumeroOrden(cadena.substring(1,6));
+           OrdenAuxiliar.setNumeroVIN(cadena.substring(1,6));
+           OrdenAuxiliar.setDescripcionDelModelo(cadena.substring(1,6));
+           OrdenAuxiliar.setAnioModelo(cadena.substring(1,6));
+           OrdenAuxiliar.setActividadDelVehiculo(cadena.substring(1,6));
+           OrdenAuxiliar.setFechaInicioOR(cadena.substring(1,6));
+           OrdenAuxiliar.setHoraInicioOR(cadena.substring(1,6));
+           OrdenAuxiliar.setRecorridoIngreso(cadena.substring(1,6));
+           OrdenAuxiliar.setFechaFinOR(cadena.substring(1,6));
+           OrdenAuxiliar.setHoraFinOR(cadena.substring(1,6));
+           OrdenAuxiliar.setRecorridoSalida(cadena.substring(1,6));
+           OrdenAuxiliar.setUnidadMedida(cadena.substring(1,6));
+           OrdenAuxiliar.setDescripcionDefecto(cadena.substring(1,6));
+           OrdenAuxiliar.setTipoCliente(cadena.substring(1,6));
+           OrdenAuxiliar.setRazonSocial(cadena.substring(1,6));
+           OrdenAuxiliar.setNombre(cadena.substring(1,6));
+           OrdenAuxiliar.setCuitEmpresa(cadena.substring(1,6));
+           OrdenAuxiliar.setDireccion(cadena.substring(1,6));
+           OrdenAuxiliar.setLocalidad(cadena.substring(1,6));
+           OrdenAuxiliar.setCodigoPostal(cadena.substring(1,6));           
+           OrdenAuxiliar.setProvincia(cadena.substring(1,6));           
+           OrdenAuxiliar.setPais(cadena.substring(1,6));           
+           OrdenAuxiliar.setPrefijo1(cadena.substring(1,6));           
+           OrdenAuxiliar.setTelefono1(cadena.substring(1,6));           
+           OrdenAuxiliar.setPrefijo2(cadena.substring(1,6));           
+           OrdenAuxiliar.setTelefono2(cadena.substring(1,6));         
+           OrdenAuxiliar.setEmail(cadena.substring(1,6));           
+           OrdenAuxiliar.setApellidoContacto(cadena.substring(1,6));           
+           OrdenAuxiliar.setNombreContacto(cadena.substring(1,6));           
+           OrdenAuxiliar.setPrefijoContacto(cadena.substring(1,6));           
+           OrdenAuxiliar.setTelefonoContacto(cadena.substring(1,6));           
+           OrdenAuxiliar.setTipoCargo(cadena.substring(1,6));           
+           OrdenAuxiliar.setValorManoDeObra(cadena.substring(1,6));           
+           OrdenAuxiliar.setImporteManoDeObra(cadena.substring(1,6));           
+           OrdenAuxiliar.setImportesAdicionales(cadena.substring(1,6));           
+           OrdenAuxiliar.setHorasFacturadas(cadena.substring(1,6));           
+           OrdenAuxiliar.setImportesMateriales(cadena.substring(1,6));           
+           OrdenAuxiliar.setImporteLubricantes(cadena.substring(1,6));    
+           OrdenAuxiliar.setMarca(cadena.substring(1,6));           
+           OrdenAuxiliar.setTipo(cadena.substring(1,6));           
+           OrdenAuxiliar.setPreEntrega(cadena.substring(1,6));           
+           OrdenAuxiliar.setTallerMovil(cadena.substring(1,6));           
+           
+           ordenes.add(OrdenAuxiliar);
+           
         }
         b.close();
-    } 
-    
-    /**
-     * Metodo principal de ejecucion, indica el inicio del sistema
-     * @param args
-     * @throws IOException 
-     */    
-    
+    }
+
 }
